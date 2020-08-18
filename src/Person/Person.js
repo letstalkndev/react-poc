92a75cdb3ddc231c3add38f5637276db450f3ac8
shivani <shivanisehdev86@gmail.com>
2020-08-18 08:43:27 +0530

    adding style to react components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './Person.css'
+
 const person = (props) => {
     return(
-        <div>
+        <div className='Person'>
             <p>I am {props.name} and {props.age} years old</p>
             <p>{props.children}</p>
             <input onChange={props.change} />
