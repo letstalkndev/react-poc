dbad89f7a5f93196878b71545d2b2e88beeb9fff
shivani <shivanisehdev86@gmail.com>
2020-08-21 07:09:10 +0530

    toggle view conditionally

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props => {
-    const [personsState, setPersonsState] = useState({
+class App extends Component {
+    state = {
         person: [
             {name: 'shivani', age: 28},
             {name: 'anu', age: 16},
             {name: 'koko', age: 28}
-        ]
-    });
+        ],
+        showPerson : false
+    }
 
-    const changeNameHandle = () => {
-        setPersonsState({
+    changeNameHandle = () => {
+        this.setState({
             person: [
                 {name: 'panu', age: 28},
                 {name: 'anu', age: 16},
@@ -21,8 +22,8 @@ const App = props => {
         });
     };
 
-    const changeClickHandle = (event) => {
-        setPersonsState({
+    changeClickHandle = (event) => {
+        this.setState({
             person: [
                 {name: 'panu', age: 28},
                 {name: event.target.value, age: 16},
@@ -31,21 +32,34 @@ const App = props => {
         });
     };
 
-    return (
-        <div className="App">
-            <h1>HELLO</h1>
-            <button onClick={changeNameHandle}>Click Here</button>
-            <Person
-                name={personsState.person[0].name}
-                age={personsState.person[0].age} />
-            <Person
-                name={personsState.person[1].name}
-                age={personsState.person[1].name}
-                change={changeClickHandle}>
-                Nested Function
-            </Person>
-        </div>
-    );
+    toggleHandler = () => {
+        const doesShow = this.state.showPerson;
+        this.setState({showPerson : !doesShow})
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>HELLO</h1>
+                <button onClick={this.toggleHandler}>Click Here</button>
+                {
+                    this.state.showPerson ?
+                    <div>
+                        <Person
+                            name={this.state.person[0].name}
+                            age={this.state.person[0].age} />
+                        <Person
+                            name={this.state.person[1].name}
+                            age={this.state.person[1].name}
+                            change={this.changeClickHandle}>
+                            Nested Function
+                        </Person>
+                    </div> : null
+                }
+            </div>
+        );
+    }
+
 
 }
 
